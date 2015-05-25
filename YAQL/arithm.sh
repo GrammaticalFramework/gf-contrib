89c35cbc94542c0ca0e17d73895af9468348dfa2
Aarne Ranta <aarne@chalmers.se>
2015-05-25 15:55:08 +0200

    startcat in arithm.sh

--- YAQL/arithm.sh
+++ YAQL/arithm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ghc -e "$(echo "p -lang=Eng \"$1\" | pt -number=1 \
-  | l -lang=Haskell" | gf -run QueryArithmEng.gf QueryArithmHaskell.gf)"
+  | l -lang=Haskell" | gf -run -startcat=Move QueryArithmEng.gf QueryArithmHaskell.gf)"
