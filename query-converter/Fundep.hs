4494e203a845f7bb7107fccd7ffda27a3ae87b6d
Aarne Ranta <aarne@chalmers.se>
2016-01-27 13:38:22 +0100

    added bases of functional dependencies. Not always minimal yet

--- query-converter/Fundep.hs
+++ query-converter/Fundep.hs
@@ -1,7 +1,9 @@
 module Fundep where
 
+import qualified Data.List as L
+
 -- straightforward, unoptimized algorithms for functional dependencies and normal forms
--- work fine for relations with any reasonable number of attributes
+-- works fine for relations with a reasonable number of attributes
 
 type Attr     = String
 type Fundep   = ([Attr], Attr)
@@ -14,17 +16,35 @@ closure (allAttrs, (deps,_)) attrs = iter attrs where
   iter xs = let xs2 = add xs in if xs2 == xs then xs else iter xs2
   add  xs = xs ++ [a | a <- allAttrs, not (elem a xs), follows deps xs a]
 
+-- a is a subset of b, if all elements of a occur in b
+subset :: Eq a => [a] -> [a] -> Bool
+subset a b = all (\x -> elem x b) a
+
 -- whether a is determined by xs, given deps
 follows :: [Fundep] -> [Attr] -> Attr -> Bool
-follows deps xs a = or [all (flip elem xs) args | (args,b) <- deps, b == a]
+follows deps xs a = or [subset args xs | (args,b) <- deps, b == a]
 
 -- the transitive closure of functional dependencies
 closureFundep :: Relation -> [Fundep]
-closureFundep rel@(allAttrs,(deps,_)) = [(xs, a) | xs <- subsets allAttrs, a <- closure rel xs, notElem a xs] 
+closureFundep rel@(allAttrs,(deps,_)) = [(xs, a) | xs <- subsets allAttrs, a <- closure rel xs, notElem a xs]
+
+-- a minimal basis of functional dependencies FD has the FD in its closure but no smaller one does
+basesFundep ::  Relation -> [[Fundep]]
+basesFundep rel@(attrs,(fundep,mvd)) = L.nub [fd |
+  fd <- minimizes,
+  sufficient fd
+  ]
+ where
+   minimizes      = concatMap subsets (sequence (map weakenings nontrivials))
+   nontrivials    = [fd | fd <- fundep, not (isTrivialFundep rel fd)]
+   weakenings (xs,a) = [(x,a) | x <- subsets xs, not (null x)] 
+   sufficient fd  = subset origClosureRel (closureFundep (attrs,(fd,mvd)))
+   origClosureRel = closureFundep rel
+   ---- TODO: minimize
 
 -- attrs is a superkey, if allAttrs is included in the closure of attrs
 isSuperkey :: Relation -> [Attr] -> Bool
-isSuperkey rel@(allAttrs, (deps,_)) attrs = all (flip elem (closure rel attrs)) allAttrs
+isSuperkey rel@(allAttrs, (deps,_)) attrs = subset allAttrs (closure rel attrs)
 
 -- attrs is a key, if it is a superkey
 --                    and any list where any of its attributes is removed is no longer a superkey  
@@ -33,7 +53,7 @@ isKey rel@(allAttrs,(deps,_)) attrs = isSuperkey rel attrs
                                && not (any (isSuperkey rel) [filter (/=a) attrs | a <- attrs])
 
 -- find all subsets to test e.g. what keys there are
-subsets :: [Attr] -> [[Attr]]
+subsets :: [a] -> [[a]]
 subsets attrs = map concat (sequence [[[],[a]] | a <- attrs])
 
 -- all superkeys
@@ -48,9 +68,16 @@ keys rel@(allAttrs,(deps,_)) = [attrs | attrs <- superkeys rel, isKey rel attrs]
 isPrime :: Relation -> Attr -> Bool
 isPrime rel attr = elem attr (concat (keys rel))
 
+-- an FD X ->> A is trivial if A is an element of X
+isTrivialFundep :: Relation -> Fundep -> Bool
+isTrivialFundep rel@(attrs,_) fd@(xs,a) = elem a xs
+
 -- find violations of the Boyce-Codd Normal Form
 violateBCNF :: Relation -> [Fundep]
-violateBCNF rel@(_,(deps,_)) = [dep | dep@(args,val) <- closureFundep rel, not (isSuperkey rel args)]
+violateBCNF rel@(_,(deps,_)) = [dep |
+  dep@(args,val) <- closureFundep rel,
+  not (isTrivialFundep rel dep),
+  not (isSuperkey rel args)]
 
 -- check if a relation is in the Boyce-Codd Normal Form
 isBCNF :: Relation -> Bool
@@ -72,10 +99,10 @@ violate4NF rel@(_,(deps,mvds)) = [dep |
   not (isSuperkey rel xs),
   not (isTrivialMultidep rel dep)]
 
-
+-- an MVD X ->> Y is trivial if Y includes X or if X,Y include all the attributes 
 isTrivialMultidep :: Relation -> Multidep -> Bool
 isTrivialMultidep rel@(attrs,_) mvd@(xs,ys) =
-  all (\y -> elem y xs) ys || all (\a -> elem a (xs ++ ys)) attrs
+  subset ys xs || subset attrs (xs ++ ys)
 
 -- check if a relation is in the Fourth Normal Form
 is4NF :: Relation -> Bool
@@ -97,14 +124,23 @@ normalizeBCNF rel = case violateBCNF rel of
 restrictRel :: Relation -> [Attr] -> Relation
 restrictRel rel@(_,(fundeps,mvds)) attrs =
   (attrs,
-   ([(xs,a)  | (xs,a)  <- closureFundep rel,    all (flip elem attrs) (a:xs)],
-    [(xs,ys) | (xs,ys) <- closureMultidep rel,  all (flip elem attrs) (ys ++ xs)])) 
+   ([(xs,a)  | (xs,a)  <- closureFundep rel,    subset (a:xs) attrs],
+    [(xs,ys) | (xs,ys) <- closureMultidep rel,  subset (ys ++ xs) attrs])) 
+
+-- bring relation to 3NF
+normalize3NF :: Relation -> [Relation]
+normalize3NF rel@(attrs,(fundeps,_)) =
+    [rel] ---- TODO
+  where
+    basis = case basesFundep rel of
+      b:_ -> b                     --  take any basis - why not the first one
+      _ -> error "no basis found"  --- should not happen
 
 -- bring relation to 4NF
 normalize4NF :: Relation -> [Relation]
 normalize4NF rel = case violate4NF rel of
   [] -> [rel]
-  f:fs -> concatMap normalize4NF (decompose f rel) ----
+  f:fs -> concatMap normalize4NF (decompose f rel)
  where
   decompose (xs,ys) rel@(attrs,(fundeps,_)) =
     let 
@@ -114,7 +150,6 @@ normalize4NF rel = case violate4NF rel of
 
 closureMultidep :: Relation -> [Multidep]
 closureMultidep rel@(_,(fundeps,mvds)) = mvds ---- TODO
-
 
 ------------ printing and parsing
 
@@ -141,10 +176,11 @@ prMultidep :: Multidep -> String
 prMultidep (xs,ys) = unwords (xs ++ ["->>"] ++ ys)
 
 prRelation :: Relation -> String
-prRelation (attrs,(fundeps,mvds)) = unlines [
+prRelation rel@(attrs,(fundeps,mvds)) = unlines [
   "Attributes:",
   unwords attrs,
-  "",
+  "Keys:",
+  unlines (map unwords (keys rel)),
   "Functional dependencies:",
   if null fundeps then "none" else unlines (map prFundep fundeps),
   if null mvds then ""
@@ -154,6 +190,8 @@ prRelation (attrs,(fundeps,mvds)) = unlines [
 prRelationInfo :: Relation -> String
 prRelationInfo rel@(attrs,(fundeps,mvds)) = unlines [
   prRelation rel,
+  "Bases of functional dependencies:",
+  unlines [unlines ([]:map prFundep fds) | fds <- basesFundep rel],
   "Derived functional dependencies:",
   unlines (map prFundep (filter (flip notElem fundeps) (closureFundep rel))),
   "Superkeys:",
