7bda02fe06662ae6626dea158ee0843386d521c3
Aarne Ranta <aarne@chalmers.se>
2016-03-02 13:46:09 +0100

    investigating XPath

--- query-converter/XPath.hs
+++ query-converter/XPath.hs
@@ -2,7 +2,7 @@
 
 module XPath where
 
-import ValidateXML (printXML) ---
+import ValidateXML 
 
 import AbsXML
 import ParXML
@@ -21,6 +21,43 @@ queryXPath xp docs = getXPValues $ concatMap (pathsXPath xp) [el | DXML _ _ el <
 
 getXPValues = XPElements ----
 
+data XPValue = XPElements [Element] | XPAttributes [String]
+  deriving Show
+
+prXPValue :: XPValue -> String
+prXPValue v = case v of
+  XPElements es -> unlines $ map printXML es
+  XPAttributes vs -> unwords vs
+
+children :: Element -> [Element]
+children = contentsElement
+
+descendants :: Element -> [Element]
+descendants e = case children e of
+  ce@(_:_) -> ce ++ concatMap descendants ce
+  _ -> []
+
+matchXPath :: XPath -> Element -> [Element]
+matchXPath xp el = case xp of
+  XPCont axis item cond xp2 -> case axis of
+    XAPlain -> concatMap (match item cond) $ concatMap (matchXPath xp2) (nexts item el)
+    XADesc  -> concatMap (matchXPath (XPCont XAPlain item cond xp2)) (el : descendants el)
+  XPEnd -> [el]
+  XPAlt xp1 xp2 -> matchXPath xp1 el ++ matchXPath xp2 el
+ where
+   match :: XItem -> XCond -> Element -> [Element]
+   match item cond el = case item of
+     XIElem t -> if typeElement el == t then [el] else []
+     XIAttr a -> [EData (WIdent (Ident s)) | (b,s) <- attributesElement el, b == a]
+     _ -> [] ---- TODO
+   nexts :: XItem -> Element -> [Element]
+   nexts item el = case item of
+     XIElem t -> if typeElement el == t then children el else []
+     XIAny    -> children el
+     _ -> [] ---- TODO
+
+
+
 ---- only works for plain elements so far
 pathsXPath :: XPath -> Element -> [Element]
 pathsXPath xp elm = case (xp,elm) of
@@ -34,12 +71,3 @@ pathsXPath xp elm = case (xp,elm) of
     XIElem i | i == name -> concat [vs | el <- elems, vs <- [pathsXPath xp2 el]]
     _ -> []
   (XPEnd,_) -> [elm]
-
-data XPValue = XPElements [Element] | XPAttributes [String]
-  deriving Show
-
-prXPValue :: XPValue -> String
-prXPValue v = case v of
-  XPElements es -> unlines $ map printXML es
-  XPAttributes vs -> unwords vs
-
