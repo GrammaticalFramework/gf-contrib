a4ee64bc69faa7bd939440a2330151d531428cd3
Aarne Ranta <aarne@chalmers.se>
2016-03-07 20:54:48 +0100

    some fixes in XPath condition matching; still not complete

--- query-converter/XPath.hs
+++ query-converter/XPath.hs
@@ -9,7 +9,7 @@ import ParXML
 import LexXML
 
 import ErrM
-import qualified Data.Map as M
+import Data.Char (isDigit)
 
 execXPath :: String -> [Document] -> IO () 
 execXPath s docs = putStrLn $ case pXPath (myLexer s) of
@@ -47,11 +47,40 @@ matchXPath xp el = case xp of
      XPEnd -> [el]
      _ -> concatMap (matchXPath xp2) els
    nexts :: XPath -> XItem -> XCond -> Element -> [Element]
-   nexts xp2 item cond el = case item of
-     XIElem t -> if (typeElement el == t) then matches el xp2 (children el) else []
+   nexts xp2 item cond el = appCond cond $ case item of
+     XIElem t -> case xp2 of
+       XPCont XAPlain (XIAttr a) _ XPEnd | (typeElement el == t) -> [EData (WIdent (Ident s)) | (b,s) <- attributesElement el, b == a]
+       XPCont XAPlain XIAnyAt    _ XPEnd | (typeElement el == t) -> [EData (WIdent (Ident s)) | (b,s) <- attributesElement el]
+       _ | (typeElement el == t) -> matches el xp2 (children el)
+       _ -> []
      XIAttr a -> [EData (WIdent (Ident s)) | (b,s) <- attributesElement el, b == a]
+     XIAnyAt  -> [EData (WIdent (Ident s)) | (b,s) <- attributesElement el]
      XIAny    -> matches el xp2 (children el)
      XINone   -> matches el xp2 (children el) --- can only appear last in a path, but the syntax is more liberal
 
----- TODO conditions; check if attributes are matched on the right level
+   -- the rest is about conditions
+   appCond :: XCond -> [Element] -> [Element]
+   appCond cond els = case cond of
+     XCOrd i     -> take 1 $ drop (fromInteger i - 1) els  -- [1] selects the first one
+     XCOp x op y -> [el | el <- els, matchOp op (valExp el x) (valExp el y)] 
+     _ -> els
+   matchOp :: XOp -> String -> String -> Bool
+   matchOp op = case op of
+     XOEq  -> (==)
+     XONEq -> (/=)
+     XOLt  -> numOp (<) (<)
+     XOGt  -> numOp (>) (>)
+     XOLEq -> numOp (<=) (<=)
+     XOGEq -> numOp (>=) (>=)
+   valExp :: Element -> XExp -> String
+   valExp el exp = case exp of
+     XEIdent a -> head $ [s | (b,s) <- attributesElement el, b == a]
+     XEInt i   -> show i
+     XEStr s   -> s
+   numOp :: (Int -> Int -> Bool) -> (String -> String -> Bool) -> String -> String -> Bool
+   numOp op sop x y
+     | all isDigit (x++y) = op (read x) (read y)
+     | otherwise = sop x y
+
+---- TODO conditions
 
