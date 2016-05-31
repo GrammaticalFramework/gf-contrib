f85dfe905ecfea3b46297617e7e203a189dae537
Kevin Kofler <kofler@dagopt.com>
2016-05-31 17:47:42 +0200

    c-bindings: Fix the build
    Fix the build of the old C bindings to the Haskell runtime. They will
    probably not be used much now that there is a native C runtime, but it
    was easy to get them to compile again:
    
    build-gfctest.sh: Fix the path to QueryEng.gf to match the gf-contrib
                      repository layout. Also mark the script as executable.
    
    PGFFFI.hs: Update the imports for the current Haskell standard.

--- c-bindings/build-gfctest.sh
+++ c-bindings/build-gfctest.sh
@@ -17,6 +17,6 @@
 # License along with this library; if not, see <http://www.gnu.org/licenses/>.
 src=../../src
 import=-i$src/runtime/haskell:$src/compiler
-gf --make ../../examples/tutorial/embedded/QueryEng.gf &&
+gf --make ../tutorial/embedded/QueryEng.gf &&
 ghc $import --make -fglasgow-exts -O2 -no-hs-main $* -c PGFFFI.hs &&
 ghc $import --make -fglasgow-exts -O2 -no-hs-main $* gfctest.c gf_lexing.c PGFFFI.hs -o gfctest
