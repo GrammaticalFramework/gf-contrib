545785c59b9d5d1457133244a05f2408a624c982
Normunds Grūzītis <normundsg@gmail.com>
2016-10-30 01:49:09 +0300

    AMR-to-text: roles and functions of PropBank frames

--- AMR/AMR-to-text/tregex/src/org/grammaticalframework/amr/propbank/FrameExtractor.java
+++ AMR/AMR-to-text/tregex/src/org/grammaticalframework/amr/propbank/FrameExtractor.java
@@ -31,25 +31,35 @@ public class FrameExtractor {
 		}
 	}
 	
-	private static List<String> processRoleSet(Element roleset) throws Exception {
+	private static List<List<String>> processRoleSet(Element roleset) throws Exception {
+		List<List<String>> types = new ArrayList<List<String>>();
+		types.add(new ArrayList<String>()); // Entries
+		types.add(new ArrayList<String>()); // Roles
+				
 		XPath xpath = XPathFactory.newInstance().newXPath();
-		NodeList aliases = (NodeList) xpath.evaluate("aliases/alias", roleset, XPathConstants.NODESET);
 		
-		List<String> entries = new ArrayList<String>();
+		NodeList entries = (NodeList) xpath.evaluate("aliases/alias", roleset, XPathConstants.NODESET);
 		
-		for (int i = 0; i < aliases.getLength(); i++) {
-			Element a = (Element) aliases.item(i);
-			String pos = convertType(a.getAttribute("pos"));
+		for (int i = 0; i < entries.getLength(); i++) {
+			Element e = (Element) entries.item(i);
+			String pos = convertType(e.getAttribute("pos"));
 			
 			if (pos != null) {
-				entries.add(a.getTextContent() + "_" + pos);
+				types.get(0).add(e.getTextContent() + "_" + pos);
 			}
 		}
 		
-		return entries;
+		NodeList roles = (NodeList) xpath.evaluate("roles/role", roleset, XPathConstants.NODESET);
+		
+		for (int i = 0; i < roles.getLength(); i++) {
+			Element r = (Element) roles.item(i);
+			types.get(1).add("ARG" + r.getAttribute("n") + "=" + r.getAttribute("f").toUpperCase());
+		}
+		
+		return types;
 	}
 	
-	private static void processFrameSet(File file, Map<String, List<String>> framesets) throws Exception {
+	private static void processFrameSet(File file, Map<String, List<List<String>>> framesets) throws Exception {
 		if (!file.getName().toLowerCase().endsWith("xml")) {
 			return;
 		}
@@ -64,14 +74,14 @@ public class FrameExtractor {
 		
 		for (int i = 0; i < rolesets.getLength(); i++) {
 			Element rs = (Element) rolesets.item(i);
-			List<String> aliases = processRoleSet(rs);
+			List<List<String>> types = processRoleSet(rs);
 			
-			framesets.put(rs.getAttribute("id").replace('.', '-'), aliases);
+			framesets.put(rs.getAttribute("id").replace('.', '-'), types);
 		}
 	}
 	
-	public static Map<String, List<String>> extract(String path) throws Exception {
-		Map<String, List<String>> framesets = new TreeMap<String, List<String>>();
+	public static Map<String, List<List<String>>> extract(String path) throws Exception {
+		Map<String, List<List<String>>> framesets = new TreeMap<String, List<List<String>>>();
 		
 		File dir = new File(path);
 		if (dir.isDirectory()) {
@@ -85,16 +95,21 @@ public class FrameExtractor {
 	}
 	
 	public static void main(String[] args) throws Exception {
-		Map<String, List<String>> framesets = extract("../lexicons/propbank/frames");
+		Map<String, List<List<String>>> framesets = extract("../lexicons/propbank/frames");
 		
-		PrintWriter out = new PrintWriter("../lexicons/propbank/frames-entries.txt", "UTF-8");
+		PrintWriter frame_entries = new PrintWriter("../lexicons/propbank/frames-entries.txt", "UTF-8");
+		PrintWriter frame_roles = new PrintWriter("../lexicons/propbank/frames-roles.txt", "UTF-8");
 		
 		for (String frame : framesets.keySet()) {
-			List<String> alter = framesets.get(frame);
-			out.println(frame + "\t" + alter.toString().replaceAll("[\\[ \\]]", ""));
+			List<String> entries = framesets.get(frame).get(0);
+			List<String> roles = framesets.get(frame).get(1);
+			
+			frame_entries.println(frame + "\t" + entries.toString().replaceAll("[\\[ \\]]", ""));
+			frame_roles.println(frame + "\t" + roles.toString().replaceAll("[\\[ \\]]", ""));
 		}
 		
-		out.close();
+		frame_entries.close();
+		frame_roles.close();
 	}
 	
 }
