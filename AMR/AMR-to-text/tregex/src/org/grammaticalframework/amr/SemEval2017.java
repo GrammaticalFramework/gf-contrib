811adf3b47437f6f0eb2ee258d3fbe8ee3baa842
Normunds Grūzītis <normundsg@gmail.com>
2016-12-03 00:44:27 +0200

    AMR-to-text: preparing for SemEval

--- AMR/AMR-to-text/tregex/src/org/grammaticalframework/amr/SemEval2017.java
+++ AMR/AMR-to-text/tregex/src/org/grammaticalframework/amr/SemEval2017.java
@@ -3,10 +3,13 @@ package org.grammaticalframework.amr;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.grammaticalframework.amr.tregex.Transformer;
+
+import edu.stanford.nlp.util.Pair;
 
 public class SemEval2017 {
 
@@ -15,12 +18,12 @@ public class SemEval2017 {
      * @param path
      * @return
      */
-    public static List<String> readAMRs(File path) throws Exception {
+    public static List<Pair<String, String>> readAMRs(File path) throws Exception {
         if (!path.isDirectory()) {
             return null;
         }
 
-        List<String> amrs = new ArrayList<String>();
+        List<Pair<String, String>> amrs = new ArrayList<Pair<String, String>>();
         StringBuilder amr = new StringBuilder();
 
         File[] files = path.listFiles();
@@ -32,6 +35,7 @@ public class SemEval2017 {
 
             BufferedReader input = new BufferedReader(new FileReader(f));
             String line = null;
+            String snt = null;
             int count = 0;
 
             while ((line = input.readLine()) != null) {
@@ -40,10 +44,12 @@ public class SemEval2017 {
                 if (line.startsWith("# ::snt")) {
                     // Add previous AMR (if any) to the list
                     if (amr.length() > 0) {
-                        amrs.add(amr.toString().trim());
+                        amrs.add(new Pair<String, String>(snt, amr.toString().trim()));
                         amr.setLength(0);
                         count++;
                     }
+
+                    snt = line.substring("# ::snt".length()).trim();
                 } else if (line.startsWith("#")) {
                     continue;
                 } else if (!line.isEmpty()) {
@@ -53,17 +59,17 @@ public class SemEval2017 {
 
             // Add last AMR to the list
             if (amr.length() > 0) {
-                amrs.add(amr.toString().trim());
+                amrs.add(new Pair<String, String>(snt, amr.toString().trim()));
                 amr.setLength(0);
                 count++;
             }
 
             input.close();
 
-            System.out.println("# " + f.getName() + ": " + count);
+            System.out.println(f.getName() + ": " + count);
         }
 
-        System.out.println("# Total: " + amrs.size() + "\n");
+        System.out.println("Total: " + amrs.size() + "\n");
 
         return amrs;
     }
@@ -75,15 +81,25 @@ public class SemEval2017 {
     public static void main(String[] args) throws Exception {
         Transformer amr2gf = new Transformer("../rules/amr2api.tsurgeon", "../lexicons/propbank/frames-roles.txt");
 
-        List<String> amrs = readAMRs(new File("../amrs/"));
+        List<Pair<String, String>> amrs = readAMRs(new File("../amrs/"));
 
-        for (String amr : amrs) {
-            System.out.println("AMR: " + amr);
+        PrintWriter answer_ext = new PrintWriter("out/answer-extended.txt", "UTF-8");
+        PrintWriter answer = new PrintWriter("out/answer.txt", "UTF-8");
 
-            String ast = amr2gf.transformToGF(amr2gf.transformToLISP(amr)).get(0);
+        for (Pair<String, String> amr : amrs) {
+            answer_ext.println("SNT: " + amr.first);
+            answer_ext.println("AMR: " + amr.second);
 
-            System.out.println("AST: " + ast + "\n");
+            String ast = amr2gf.transformToGF(amr2gf.transformToLISP(amr.second)).get(0);
+
+            answer_ext.println("AST: " + ast);
+            answer_ext.println("TXT: " + "\n"); // TODO
+
+            answer.println("TXT: "); // TODO
         }
+
+        answer_ext.close();
+        answer.close();
     }
 
 }
