ef5d55de37a4942e9f32ba59644cfba621c4d18b
Normunds Grūzītis <normundsg@gmail.com>
2017-01-07 12:51:24 +0200

    AMR-to-text: a quick post-editing bug-fix

--- AMR/AMR-to-text/tregex/src/org/grammaticalframework/amr/Processor.java
+++ AMR/AMR-to-text/tregex/src/org/grammaticalframework/amr/Processor.java
@@ -303,6 +303,18 @@ public class Processor {
         snt = snt.replaceAll("\\b(?<!\\d[,.])9(?![,.]\\d)\\b", "nine");
         snt = snt.replaceAll("\\b(?<!\\d[,.])10(?![,.]\\d)\\b", "ten");
 
+        // Undo replacement for dates
+        snt = snt.replaceAll("\\bone (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", "1 $1");
+        snt = snt.replaceAll("\\btwo (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", "2 $1");
+        snt = snt.replaceAll("\\bthree (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", "3 $1");
+        snt = snt.replaceAll("\\bfour (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", "4 $1");
+        snt = snt.replaceAll("\\bfive (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", "5 $1");
+        snt = snt.replaceAll("\\bsix (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", "6 $1");
+        snt = snt.replaceAll("\\bseven (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", "7 $1");
+        snt = snt.replaceAll("\\beight (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", "8 $1");
+        snt = snt.replaceAll("\\bnine (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", "9 $1");
+        snt = snt.replaceAll("\\bten (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", "10 $1");
+
         snt = snt.substring(0, 1).toUpperCase() + snt.substring(1); // Capitalize first letter
         snt = snt.replaceAll(" ([,.!?])", "$1"); // Remove spaces before punctuation
         snt = snt.replaceAll("[.][.]$", "."); // Remove a redundant full-stop
