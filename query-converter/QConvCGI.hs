86bf8208232313c258a268325c96c573d97361e2
Thomas Hallgren <th-github@altocumulus.org>
2018-01-24 14:28:30 +0100

    Move qconv web interface to new subdirectory www Add index.html qconv.css in www. The web application can be installed by running 'make qconv.cgi' and then copying the www directory to the server.

--- query-converter/QConvCGI.hs
+++ query-converter/QConvCGI.hs
@@ -23,17 +23,17 @@ qconvCGI cmd =
                 setHeader "Content-Type" "text/html"
                 output $
                   h3 "E-R diagram" ++ svg ++
-                  h3 "Database schema" ++ "<pre class=schema>" ++
-                  prSchema (erdiagram2schema SER e) ++ "</pre>"++
+                  h3 "Database schema" ++
+                  pre_cls "schema" (prSchema (erdiagram2schema SER e)) ++
                   h3 "In English (not necessarily perfect)"++
-                  bullets (erdiagram2text e)
+                  bullets "schema" (lines (erdiagram2text e))
       "nf" -> do src <- getRequiredInput "file"
                  let rel = pRelation (lines src)
                  setHeader "Content-Type" "text/html"
                  output$
                    h3 "Dependencies and keys" ++
-                   pre (prRelationInfo rel) ++
-                   unlines [h3 hdr ++pre txt | (hdr,txt)<-prNormalizations rel]
+                   pre_cls "nf" (prRelationInfo rel) ++
+                   unlines [h3 hdr ++pre_cls "nf" txt | (hdr,txt)<-prNormalizations rel]
       "hello" -> do setHeader "Content-Type" "text/plain"
                     output "Hello!\n"
       _ -> outputError 400 "Bad request" ["Unknown command: "++cmd]
@@ -41,11 +41,16 @@ qconvCGI cmd =
 
 getRequiredInput name = maybe (missing name) return =<< getInput name
 
+----
 
-bullets = unlines . ("<ul>":) . (++["</ul>"]) . map ("<li>"++) . lines
+bullets c = wrap_cls "ul" c . unlines . map ("<li>"++)
 
-h3 s = "<h3>"++s++"</h3>\n"
-pre s = "<pre>"++s++"</pre>\n"
+h3 = wrap "h3"
+pre = wrap "pre"
+pre_cls = wrap_cls "pre"
+
+wrap tag s = "<"++tag++">\n"++s++"</"++tag++">\n"
+wrap_cls tag cls s = "<"++tag++" class="++cls++">\n"++s++"</"++tag++">\n"
 
 --------------------------------------------------------------------------------
 -- * General CGI Error exception mechanism
