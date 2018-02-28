ee2bd22c7432d3b54ee0f3d55ebed224cc1c620a
Thomas Hallgren <th-github@altocumulus.org>
2018-02-28 17:22:43 +0100

    QConv web interface: add Save button for E-R diagrams

---
+++ query-converter/www/qconv.js
@@ -0,0 +1,51 @@
+
+function insertAfter(el,ref) {
+    ref.parentNode.insertBefore(el,ref.nextSibling);
+}
+
+function node(tag,as,ds) {
+    var n=attr(as,document.createElement(tag));
+    if(ds) for(var i in ds) n.appendChild(ds[i]);
+    return n;
+}
+
+function attr(as,n) {
+    for(var a in as) n.setAttribute(a,as[a]);
+    return n
+}
+
+function uploadFileTo(file) {
+    var files=browse.files
+    if(files.length==1) {
+        var reader=new FileReader()
+        reader.readAsText(files[0])
+        reader.onload=function() { file.value=reader.result }
+    }
+}
+
+function postForm(form,callback) {
+    var fd=new FormData(form)
+    var http=new XMLHttpRequest()
+    http.onload=function() { callback(http.responseText) }
+    http.open("POST",form.action)
+    http.send(fd)
+    return false
+}
+
+function saveButton(text,filename,type) {
+    var saveBtn=node("input",{type:"button",value:"Save"})
+    saveBtn.onclick=function() {
+	var file=new Blob([text],{type:type})
+	var a=node("a")
+	var url=URL.createObjectURL(file)
+	a.href=url
+	a.download=filename
+	document.body.appendChild(a)
+	a.click()
+	setTimeout(function(){
+	    document.body.removeChild(a)
+	    URL.revokeObjectURL(url)
+	},0)
+    }
+    return saveBtn
+}
